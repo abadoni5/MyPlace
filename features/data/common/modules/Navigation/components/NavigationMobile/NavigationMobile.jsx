0fd1eede21e79ea5f1b9f1fbb1a479c25aad2fda
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-07-05 22:13:44 +0530

    added mobile navigation

--- features/data/common/modules/Navigation/components/NavigationMobile/NavigationMobile.jsx
+++ features/data/common/modules/Navigation/components/NavigationMobile/NavigationMobile.jsx
@@ -1,9 +1,45 @@
+import { Box, Flex, Text, Button, Menu, MenuList, MenuButton, MenuItem, IconButton } from "@chakra-ui/react"
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { HiHomeModern } from 'react-icons/hi2'
+import Link from 'next/link'
+import { navigationLinks } from 'features/data/common/modules/Navigation/components/NavigationDesktop/navigationConsts.js'
+
 const NavigationMobile = () => {
-  return (
-    <div>
-      NM
-    </div>
-  )
+    return (
+        <div>
+            <Box color="#896991" padding='2rem' backgroundColor="white" display={{ base: "block", md: "none" }}>
+                <Flex alignItems="center" justifyContent="space-between"  >
+                    <Link href="/ ">
+                        <Box display="flex" alignItems="center" gap="2">
+                            <HiHomeModern />
+                            <Text fontSize="2xl" fontWeight="black">
+                                MyPlace
+                            </Text>
+                        </Box>
+                    </Link>
+                    <Menu>
+                        <MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" />
+                        <MenuList>
+                            {navigationLinks.map((item) => (
+                                <NavigationLink {...item} />
+                            ))}
+                        </MenuList>
+                    </Menu>
+                </Flex>
+            </Box>
+        </div>
+    )
 }
 
 export default NavigationMobile
+
+const NavigationLink = ({ title, link, icon }) => {
+    return (
+        <Link href={link}>
+            <MenuItem alignItems="center" gap="0.5rem">
+                {icon}
+                {title}
+            </MenuItem>
+        </Link>
+    )
+}
