81168770c4fe15aae805b35d02d97d6f9111ee48
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-07-06 15:59:20 +0530

    added responsive footer

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navigation from 'features/data/common/modules/Navigation/Navigation.js'
+import Footer from 'features/data/common/modules/Footer/Footer.jsx'
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <Footer />
     </>
   )
 }
---
+++ features/data/common/modules/Footer/Footer.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { services, about, workWithUs, ourOffices } from './footerConsts'
+import { SimpleGrid, Text, Link, Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+
+const Footer = () => {
+    return (
+        <Box backgroundColor="#896991">
+            <Box
+                maxWidth="1280px" margin="0 auto" paddingY="3rem" paddingX={{ base: "2rem", sm: "0" }}
+            >
+                <SimpleGrid
+                    columns='4'
+                    color="whiteAlpha.700"
+                    gap='1.7rem'
+                    minChildWidth="150px"
+                >
+                    <Flex flexDirection="column">
+                        <FooterHeader title="Services" />
+                        {services.map((item) => (
+                            <FooterLink {...item} />
+                        ))}
+                    </Flex>
+
+
+                    <Flex flexDirection="column">
+                        <FooterHeader title="About" />
+                        {about.map((item) => (
+                            <FooterLink {...item} />
+                        ))}
+                    </Flex>
+
+
+                    <Flex flexDirection="column">
+                        <FooterHeader title="Work With Us" />
+                        {workWithUs.map((item) => (
+                            <FooterLink {...item} />
+                        ))}
+                    </Flex>
+
+
+                    <Flex flexDirection="column">
+                        <FooterHeader title="Our Offices" />
+                        {ourOffices.map((item) => (
+                            <FooterLink {...item} />
+                        ))}
+                    </Flex>
+                </SimpleGrid>
+            </Box>
+        </Box>
+    )
+}
+
+export default Footer
+
+const FooterLink = ({ name, link }) => {
+    return (
+        <Text>
+            <Link href={link}>
+                {name}
+            </Link>
+        </Text>
+    )
+}
+
+const FooterHeader = ({ title }) => {
+    return (
+        <Text as="h4" fontWeight="light" fontSize="xl" marginBottom="1rem">{title}</Text>
+    )
+}
