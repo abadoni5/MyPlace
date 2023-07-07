0f6c2b8a04fcad1f0ebf89989704c47ff378eba8
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-07-07 15:58:17 +0530

    added form on hero banner

--- features/Home/Components/HeroBanner/HeroBanner.jsx
+++ features/Home/Components/HeroBanner/HeroBanner.jsx
@@ -1,45 +1,54 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, Fade } from '@chakra-ui/react';
+import HeroForm from './HeroForm';
 
 const HeroBanner = () => {
     const imageUrl = 'https://images.unsplash.com/photo-1556912173-3bb406ef7e77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
 
     return (
         <>
-            <Box
-                position="relative"
-                minHeight={{ base: '110vh', sm: '60vh' }}
-                background={`url(${imageUrl})`}
-                backgroundSize="cover"
-                backgroundPosition="center"
-                backgroundAttachment="fixed"
-            >
-                <Box position="absolute" width="100%" height="100%" opacity='0.7' backgroundColor="blue.900" />
-                <Box display='flex' flexDirection={{ base: 'column', sm: 'row' }} alignItems="center"
-                    justifyContent={{ base: 'flex-start', sm: 'space-between' }}
-                    maxWidth="1280px"
-                    position="absolute"
-                    color="white"
-                    fontWeight="light"
-                    left="0"
-                    right="0"
-                    bottom="0"
-                    top="0"
-                    padding="2rem"
+            <Fade in>
+                <Box
+                    position="relative"
+                    minHeight={{ base: '110vh', sm: '60vh' }}
+                    background={`url(${imageUrl})`}
+                    backgroundSize="cover"
+                    backgroundPosition="center"
+                    backgroundAttachment="fixed"
                 >
-                    <Box width={{ base: '100%', sm: '50%' }}>
-                        <Text fontSize={{ base: '4xl', sm: '5xl' }}
-                            lineHeight='shorter'
-                            marginBottom='1.5rem'
-                        >
-                            Find your <strong style={{ color: 'black' }}>Perfect Place</strong> to stay today on <strong style={{ color: 'black' }}>MyPlace</strong>
-                        </Text>
-                    </Box>
-                    <Box width={{ base: '100%', sm: "auto" }}>
-                        Form
+                    <Box position="absolute" width="100%" height="100%" opacity='0.7' backgroundColor="blue.900" />
+                    <Box display='flex' flexDirection={{ base: 'column', sm: 'row' }} alignItems="center"
+                        justifyContent={{ base: 'flex-start', sm: 'space-between' }}
+                        maxWidth="1280px"
+                        position="absolute"
+                        color="white"
+                        fontWeight="light"
+                        left="0"
+                        right="0"
+                        bottom="0"
+                        top="0"
+                        padding="2rem"
+                    >
+                        <Box width={{ base: '100%', sm: '50%' }}>
+                            <Text fontSize={{ base: '4xl', sm: '5xl' }}
+                                lineHeight='shorter'
+                                marginBottom='1.5rem'
+                            >
+                                Find your <strong style={{ color: 'black' }}>Perfect Place</strong> to stay today on <strong style={{ color: 'black' }}>MyPlace</strong>
+                            </Text>
+                            <Text fontSize={{ base: 'lg', sm: '2xl' }}
+                                lineHeight='short'
+                                marginBottom='1.5rem'
+                            >
+                                Find Your Dream Home. Discover Ideal Cities. Know the Cost of Living.
+                            </Text>
+                        </Box>
+                        <Box width={{ base: '100%', sm: "auto" }} marginTop={{base: '2rem', sm: '0 '}}>
+                            <HeroForm />
+                        </Box>
                     </Box>
                 </Box>
-            </Box>
+            </Fade>
         </>
     );
 };
---
+++ features/Home/Components/HeroBanner/HeroForm/HeroForm.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Box, Text, FormControl, Input, Flex, Checkbox, Button } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
+
+const HeroForm = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => console.log(data);
+
+    return (
+        <Box width="100%"
+            padding="2rem"
+            borderRadius="small"
+            backgroundColor="white"
+            color="gray.700"
+        >
+            <Text fontSize="xl"
+                fontWeight="bold"
+            >
+                Free PDF Guide
+            </Text>
+            <Text
+                fontSize="lg"
+            >
+                Complete the form below to download your free guide
+            </Text>
+            <form onSumbit={handleSubmit(onSubmit)}>
+                <FormControl>
+                    <Input marginTop='1.3rem' id="name" type="text" placeHolder="Name"
+                        {...register("name", { required: true })}
+                    />
+                    {errors.name &&
+                        (<Text fontSize="xs" color="red.400">
+                            {errors.name.type}
+                        </Text>)
+                    }
+                    <Flex gap={{ base: '0', sm: '1rem' }} flexDirection={{ base: 'column', sm: 'row' }} alignItemns="center">
+                        <Input marginTop='1.3rem' id="email" type="email" placeHolder="Email"
+                            {...register("email", { required: true })}
+                        />
+                        {errors.email &&
+                            (<Text fontSize="xs" color="red.400">
+                                {errors.email.type}
+                            </Text>)
+                        }
+                        <Input marginTop='1.3rem' id="phone" type="phone" placeHolder="Phone Number"
+                            {...register("phone", { required: true })}
+                        />
+                        {errors.phone &&
+                            (<Text fontSize="xs" color="red.400">
+                                {errors.phone.type}
+                            </Text>)
+                        }
+                    </Flex>
+                    <Checkbox marginTop="1.3rem" id="gdpr" type="checkbox" placeholder="GDPR" {...register('gdpr', { required: true })}>
+                        I agree to the terms and conditions and privacy policy
+                        {errors.gdpr && (
+                            <Text fontSize="xs" color="red.400">
+                                {errors.gdpr.type}
+                            </Text>
+                        )}
+                    </Checkbox>
+                </FormControl>
+                <Button backgroundColor="#896991" width="100%" fontSize="xl"
+                    padding="2rem" marginTop="2rem"
+                >
+                    Download Now
+                </Button>
+            </form>
+        </Box>
+    )
+}
+
+export default HeroForm
